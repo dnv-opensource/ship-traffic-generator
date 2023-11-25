95137d645abc8f89034526e161c7e2e09676e509
Claas <claas.rostock@dnv.com>
2023-11-25 01:12:39 +0100

    test_write_files.py : Improved tests to be more robust to random changes of reading sequence on GitHub runners.

--- tests/test_write_files.py
+++ tests/test_write_files.py
@@ -8,11 +8,11 @@ from trafficgen.types import Situation
 from trafficgen.write_traffic_situation_to_file import write_traffic_situations_to_json_file
 
 
-def test_write_situations(
+def test_write_situations_multiple(
     situations_folder: Path,
     output_folder: Path,
 ):
-    """Test writing traffic situations."""
+    """Test writing multiple traffic situations in one call."""
 
     situations: List[Situation] = read_situation_files(situations_folder)
     write_traffic_situations_to_json_file(situations, output_folder)
@@ -20,8 +20,24 @@ def test_write_situations(
 
     assert len(situations) == len(reread_situations)
 
+
+def test_write_situations_single(
+    situations_folder: Path,
+    output_folder: Path,
+):
+    """Test writing multiple traffic situations, each in a separate single call."""
+
+    situations: List[Situation] = read_situation_files(situations_folder)
+
     # sourcery skip: no-loop-in-tests
-    for situation, reread_situation in zip(situations, reread_situations):
+    # sourcery skip: no-conditionals-in-tests
+    for situation in situations:
+        # clean output folder
+        for file in output_folder.glob("*"):
+            if file.is_file():
+                file.unlink()
+        write_traffic_situations_to_json_file([situation], output_folder)
+        reread_situation: Situation = read_situation_files(output_folder)[0]
         # single difference between the original and the reread situation should be the
         # input_file_name field
         reread_situation.input_file_name = situation.input_file_name
